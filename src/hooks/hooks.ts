9d7a7c2fd884fc98610659b843edf846b6fbb727
mateuszcieslik <mateuszcieslik92@gmail.com>
2024-08-01 11:11:10 +0200

    Add fix

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,17 +1,17 @@
-import { BeforeAll, AfterAll } from "@cucumber/cucumber";
+import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
 import { chromium, Browser, Page } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let page: Page;
 let browser: Browser;
 
-BeforeAll(async function(){
+Before(async function(){
     browser = await chromium.launch({headless: false});
     const page = await browser.newPage();
     pageFixture.page = page;
 });
 
-AfterAll(async function(){
+After(async function(){
     await pageFixture.page.close();
     await browser.close();
 
